7318d141d3f6fc3b9aaeb21d16b9290a334ae3c3
Front_End_SuberuJoe <156342887+FrontendSuberu@users.noreply.github.com>
2025-01-11 02:43:03 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React, { useRef, useEffect, useState } from "react";
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 
 function App() {
 
